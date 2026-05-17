6851316894eba2f2073afedd213543e40a7f27ed
Stive Writi <oficialwritiai@gmail.com>
2026-05-17 16:34:04 +0200

    feat(sentry): añadir DSN de producción

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || 'https://e1a8949bd0b91d93a13ef286f2238841@o4511405664698368.ingest.de.sentry.io/4511405715030096',
   tracesSampleRate: 0.1,
   environment: process.env.NODE_ENV,
 });
